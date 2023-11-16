c778338064f95c6279264047a26200dccb135916
Keith Perkins <keith.perkins@tlgcohort.com>
2023-11-16 14:00:00 -0700

    Team6_ClassIdeas

---
+++ src/com/tacocardgame/model/Card.java
@@ -0,0 +1,18 @@
+package com.tacocardgame.model;
+
+public class Card {
+    class Card {
+        private String name;
+        private String image;
+
+        public Card(String name, String image) {
+            this.name = name;
+            this.image = image;
+        }
+
+        public void show() {
+            sout(image);
+        }
+
+    }
+}
---
+++ src/com/tacocardgame/model/Deck.java
@@ -0,0 +1,29 @@
+package com.tacocardgame.model;
+
+public class Deck {
+    class Deck {
+        private List<Card> allCards = new ArrayList<>();
+
+        // if 4, create 48 cards
+        // if 5, create 55 cards
+        // create 55 - (55 % playerCount) cards
+        // e.g., 55 % 4 = 7, so (55-7) cards; 55 % 3 = 1, so (55-1) cards; 55 % 5 = 0, so (55-0) cards
+        public Deck(int playerCount) {
+            Map<String,String> nameImageMap = new HashMap<>();
+            nameImageMap.put("cat", Files.readString(Path.of("images/cat.txt")));
+            // or use Map.of()
+            // and likewise for "goat", Files.readString(...goat...);
+
+            for (var entry : nameImageMap.entrySet()) {
+                // do this 5 times,put this line below in a for loop 1-5
+                allCards.add(new Card(entry.getKey(), entry.getValue()));
+            }
+
+            Collections.shuffle(allCards);
+        }
+
+        public Card nextCard() {
+            return allCards.remove(0);
+        }
+    }
+}
