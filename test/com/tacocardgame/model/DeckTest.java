088167ed5308929223b6128f9803b0e70a6d8cbe
CJish <christopher.ulrich@tlgcohort.com>
2023-11-19 18:29:23 -0800

    Created test classes for: Card, CardType, Deck, Pile, Round.
    Next: Experiment with autocreating test class for PlayerTest with inherited values to see what it does

---
+++ test/com/tacocardgame/model/DeckTest.java
@@ -0,0 +1,17 @@
+package com.tacocardgame.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void nextCard() {
+    }
+}
---
+++ test/com/tacocardgame/model/PileTest.java
@@ -0,0 +1,24 @@
+package com.tacocardgame.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PileTest {
+
+    @Test
+    public void addToPile() {
+    }
+
+    @Test
+    public void showTopOfPile() {
+    }
+
+    @Test
+    public void getSize() {
+    }
+
+    @Test
+    public void clearPile() {
+    }
+}
