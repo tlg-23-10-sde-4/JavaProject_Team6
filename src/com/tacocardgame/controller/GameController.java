07c605f727bd471620dd671fbb01df70dbc62d81
Keith Perkins <keith.perkins@tlgcohort.com>
2023-11-19 19:41:10 -0700

    playGame() updates

--- src/com/tacocardgame/controller/GameController.java
+++ src/com/tacocardgame/controller/GameController.java
@@ -6,8 +6,10 @@ import com.tacocardgame.view.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GameController {
@@ -35,7 +37,7 @@ public class GameController {
     private void distributeCards() {   // method that distributes cards to players from Deck #js
     }
 
-    private void createPlayers(String playerName, int i) {  // method that creates players and player number #js
+    private void createPlayers(String playerName, int i) {  // method that creates players and player number #js  //does i return npc count?
         Player player = new Player();
         player.setName(playerName);
         players.add(player);  // still need method for player.add; #js //updateName in Board #kp
@@ -63,35 +65,51 @@ public class GameController {
      */
 
     private void playGame() {
-        Card middleCard = deck.nextCard();
+        Card middleCard = deck.nextCard();  //draws the first card(top card) from the deck
         boolean gameWon = false;
+        Random random = new Random();       //create new instance of random obj for randomization npc
+        Scanner scanner = new Scanner(System.in);
 
         while (!gameWon) {
+            long StartTime = System.currentTimeMillis();        //records the start time for each player's turn
+
             for (Player currentPlayer : players) {
-                String spokenWord = currentPlayer.playerSays();
+                String spokenWord = currentPlayer.playerSays();     //gets the word spoken by the current player
                 System.out.println(currentPlayer.getName() + " says: " + spokenWord);
 
-                if (spokenWord.equalsIgnoreCase(middleCard.getName())) {
-                    // Players race to slap hands on the cards
+                if (spokenWord.equalsIgnoreCase(middleCard.getName())) {    // used to compare strings of playerSays and middle card name, ignoring case
+                    // slap protocol begins
                     boolean lastPlayerSlapped = false;
+
                     for (Player player : players) {
-                        if (player != currentPlayer) {
-                            System.out.println(player.getName() + " slaps!");
-                            lastPlayerSlapped = true;
+                        if (player != currentPlayer) {      //TODO: create isnotNPC() in Player class,
+                            if (player.isnotNPC) {
+                                System.out.println("Press the space bar to slap (any other key to skip)");
+                                String userInput = scanner.nextLine();
+                                if (userInput.equalsIgnoreCase(" ")) {
+                                    System.out.println(player.getName() + " slaps");
+                                    lastPlayerSlapped = true;
+                                }
+                            } else {
+                                // Automated players
+                                if (random.nextBoolean()) {
+                                    System.out.println(player.getName() + " slaps!");
+                                    lastPlayerSlapped = true;
+                            }
                         }
                     }
-
+                }
                     if (!lastPlayerSlapped) {
                         System.out.println(currentPlayer.getName() + " is the last to slap!");
-                        currentPlayer.addCardsToPlayerHand(deck.getMiddleStack());
+                        long endTime = System.currentTimeMillis();      //records the end time slapped for last player calculation
+                        long timeElapsed = endTime - StartTime;
+                        System.out.println("Slap time for last player: " + timeElapsed + "milliseconds");
+                        currentPlayer.addCardsToPlayerHand(deck.getMiddleStack());      //adds entire middle stack to player's set of cards
                     }
-
-                    // reset the middle stack
-                    deck.clearPile();
+                    deck.clearPile();       // reset the middle stack
                 }
             }
-
-            // Check if any player has won
+            //check if player has won
             for (Player player : players) {
                 if (player.getPlayerHand().isEmpty()) { // much simpler than creating a hasNoCards() method
                     System.out.println(player.getName() + " wins the game!");   //announceWinner(); when checkCard method shows a player's cards = 0
@@ -128,5 +146,4 @@ public class GameController {
 
         Console.blankLines(2);
     }
-}
 }
