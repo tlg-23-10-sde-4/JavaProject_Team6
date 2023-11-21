c9db88efd5e56d923fa6460fa8b6dc1e25b7ebf5
Justin Stafford <duckyabrams@gmail.com>
2023-11-21 06:01:47 -0800

    Class User - @override playerSlap() method complete

--- src/com/tacocardgame/model/User.java
+++ src/com/tacocardgame/model/User.java
@@ -28,33 +28,12 @@ public class User extends Player {
 
     @Override
     public long playerSlaps() {
-          // CJ: for testing - see if we are properly overriding the super
-        System.out.println("Scanner should load next"); // CJ: for testing
-
-        Scanner playerSlap = new Scanner(System.in);
-        String userInput = playerSlap.nextLine();
-        if (userInput.equalsIgnoreCase(" ")) {
-            result = new Date().getTime();
-        }
-        return result;
-    }
-
-    public static String userSlaps() {
+        // User input logic for slapping
         long startTime = System.currentTimeMillis();
-
         Prompter prompter = new Prompter(new Scanner(System.in));
         prompter.prompt("Press the space bar to slap: ");
-        String userInput = prompter.prompt("");
 
-        long endTime = System.currentTimeMillis(); //record the endtime
-        long slapTime = endTime - startTime;
-
-        if (userInput.equals(" ")) {
-            System.out.println(Player.getName() + " slaps");
-            return getName() + " slap time: " + startTime + "ms";
-        } else {
-            return getName() + "didnt slap";
-        }
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime; // Return slap time
     }
-
 }
