4a950da5421017f5cb3d884ba121c74e8e3a7cf3
CJish <christopher.ulrich@tlgcohort.com>
2023-11-21 01:41:47 -0800

    Deck & DeckTest

--- src/com/tacocardgame/model/Deck.java
+++ src/com/tacocardgame/model/Deck.java
@@ -12,10 +12,8 @@ public class Deck {
 
     public Deck() throws IOException {
         allCards = new ArrayList<>();
-		
+
         try {
-
-
             nameTacoMap.put(CardType.TACO, Files.readString(Path.of("resources/images/taco.txt")));
             nameTacoMap.put(CardType.CAT, Files.readString(Path.of("resources/images/cat.txt")));
             nameTacoMap.put(CardType.GOAT, Files.readString(Path.of("resources/images/goat.txt")));
@@ -51,8 +49,19 @@ public class Deck {
         return temp.get(0);
     }
 
+    public void distributeCards(ArrayList<Player> players) throws IOException {
+        Deck deck = new Deck();
+        int cardsPerPlayer = deck.getAllCards().size() / players.size();
+        for (Player player : players) {
+            for (int i = 0; i < cardsPerPlayer; i++) {
+                player.getPlayerHand().add(deck.nextCard()); // Assuming nextCard() method removes and returns the top card
+            }
+        }
+    }
+
     public String getAsciiCardType(CardType cardType){      ///get ascii art by card type
         return nameTacoMap.get(cardType);
     }
 
 }
+
